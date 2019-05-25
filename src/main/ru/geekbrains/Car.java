d350302d3a61bb2be1d6e85042a2379fe5e708fe
Dias <Dias64@mail.ru>
2019-05-25 13:47:07 +0300

    homework5

--- src/main/ru/geekbrains/Car.java
+++ src/main/ru/geekbrains/Car.java
@@ -1,5 +1,10 @@
 package ru.geekbrains;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+import static ru.geekbrains.MainClass.isWinner;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
@@ -7,6 +12,8 @@ public class Car implements Runnable {
     }
     private Race race;
     private int speed;
+    private CyclicBarrier cb;
+    private CountDownLatch cdl;
     private String name;
     public String getName() {
         return name;
@@ -14,9 +21,11 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdl) {
         this.race = race;
         this.speed = speed;
+        this.cb = cb;
+        this.cdl = cdl;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -26,11 +35,27 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        cdl.countDown();
+        if (isWinner())
+            System.out.println(this.name + " победил в гонке!");
+
     }
+
+    private synchronized boolean isWinner() {
+        if (!isWinner) {
+            isWinner = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
